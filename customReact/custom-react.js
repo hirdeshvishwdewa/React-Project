8c1e5133d2fc2691f206ff92f8d04d10b96e840d
Hirdesh Vishwdewa <hirdeshvishwdewa@gmail.com>
2024-10-04 00:49:45 +0530

    Making the customRender method dynamic

--- customReact/custom-react.js
+++ customReact/custom-react.js
@@ -1,8 +1,16 @@
 function customRender(reactElement, mainContainer) {
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute("href", reactElement.props.href);
+    // domElement.setAttribute("target", reactElement.props.target);
+    // mainContainer.appendChild(domElement);
+    
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
-    domElement.setAttribute("href", reactElement.props.href);
-    domElement.setAttribute("target", reactElement.props.target);
+    for (const prop in reactElement.props) {
+        if (prop === 'childern') continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
     mainContainer.appendChild(domElement);
 }
 const reactElement = {
