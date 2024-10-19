7e09abeb64ec211bbc11f1ea4a4d0335008205c3
Hirdesh Vishwdewa <hirdeshvishwdewa@gmail.com>
2024-10-19 20:28:38 +0530

    Tailwind proj + 04 new proj bgChanger setup

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-
+  let channel = "Chai aur code";
   let myObj = {
     username: "Hirdesh",
     age: 34
@@ -11,7 +11,8 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card channel="Chai aur code" myArr={[1,2,3]} user={myObj}></Card>
+      <Card channel={channel} myArr={[1,2,3]} user={myObj}></Card>
+      <Card channel={channel} myArr={[1,2,3]} user={myObj}></Card>
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,5 +1,4 @@
-function Card(props) {
-    console.log(props);
+function Card({channel, user}) {
     return (
         <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
         <img
@@ -11,8 +10,10 @@ function Card(props) {
         <div className="bg-white p-4 sm:p-6">
           <time dateTime="2022-10-10" className="block text-xs text-gray-500"> 10th Oct 2022 </time>
           <a href="#">
-            <h3 className="mt-0.5 text-lg text-gray-900">How to position your furniture for positivity</h3>
+            <h3 className="mt-0.5 text-lg text-gray-900">{channel}</h3>
           </a>
+          <span>User: {user.username}</span>
+          <span>Age: {user.age}</span>
           <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
             pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
@@ -24,4 +25,4 @@ function Card(props) {
     );
 }
 
-export default Card;
+export default Card; 
