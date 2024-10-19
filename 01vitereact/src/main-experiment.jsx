2a4430977b3e44a44cf070136ed099a98c1363b1
Hirdesh Vishwdewa <hirdeshvishwdewa@gmail.com>
2024-10-19 16:51:16 +0530

    main experiment file

--- 01vitereact/src/main-experiment.jsx
+++ 01vitereact/src/main-experiment.jsx
@@ -1,26 +1,36 @@
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 
-function MyApp() {
-  return (
-    <div>
-      <h1>My Experiment</h1>
-    </div>
-  )
-}
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>My Experiment</h1>
+//     </div>
+//   )
+// }
 
-const ReactElement = {
-  type: 'a',
-  props: {
-      href: 'http://google.com',
-      target: '_blank'
-  },
-  children: "Click me to visit Google!"
-}
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'http://google.com',
+//       target: '_blank'
+//   },
+//   children: "Click me to visit Google!"
+// }
 
-const anotherElement = (
-  <a href='http://googl.com' target='_blank'>Visit Google</a>
+// const anotherElement = (
+//   <a href='http://googl.com' target='_blank'>Visit Google</a>
+// )
+
+const username = "Hirdesh";
+
+const reactElement = React.createElement(
+  "a",
+  {href: "http://googl.com", target: "_blank"},
+  "Click me to visit google ",
+  username
 )
 
 createRoot(document.getElementById('root')).render(
-    anotherElement
+  reactElement
 )
